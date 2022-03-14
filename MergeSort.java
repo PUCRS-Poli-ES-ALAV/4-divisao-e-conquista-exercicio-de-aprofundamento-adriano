ee9c900f9b8caae0ed62dcf95c4d8db43bf05796
Adriano Ramos <adriano.ramos@acad.pucrs.br>
2022-03-14 20:19:25 -0300

    maxVal1 and maxVal2 methods

--- MergeSort.java
+++ MergeSort.java
@@ -4,8 +4,21 @@ public class MergeSort {
         
     }
 
+    //MARK: - Exercícios 1,2 & 3
     public void mergeSort(int[] n) {
         
+        if(n.length <= 0) {
+            return;
+        }
+
+    }
+
+    private maxVal1(int A[], int n) {
+
+    }
+
+    private maxVal2(int A[], int init, int end) {
+
     }
 
 }
